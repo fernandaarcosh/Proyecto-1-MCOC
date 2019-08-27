a3bee352f25cf3513ead262ff7cd4e3f6c5e1f9e
Benjaceroni <53712580+Benjaceroni@users.noreply.github.com>
2019-08-26 22:41:37 -0400

    Add files via upload

---
+++ Datos_Amb.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 11:51:20 2019
+
+@author: Administrador
+"""
+from matplotlib.pylab import *
+from scipy import interpolate
+import numpy
+
+archivo = open('TemperaturaAmbiente.csv')
+datos=[]
+n=0
+for i in archivo:
+    datos.append((i.split(';')[0]).split(','))#[Fecha,Hora,sen,temp]
+    cambio = datos[-1][-1][:-1]
+    #print cambio
+    datos[-1][-1]=cambio
+    #print datos[-1]
+    #print datos[n]
+    n+=1
+    
+ejeTiempo=[]
+grafico=[]
+for i in datos:    #Loop para asegurar que el grafico de la temperatura ambiente esta correcto
+    ejeTiempo.append(float(i[2]))
+    grafico.append(float(i[4]))
+#pyplot.plot(ejeTiempo,grafico[0])
+
+tempAmb=interpolate.interp1d(array(ejeTiempo),grafico,kind='slinear')  #Interpolacion de los datos
+t=linspace(0,12,10000)  #Rango de los dias de datos otorgados
+#pyplot.plot(t,tempAmb(t))
+archivo = close('TemperaturaAmbiente.csv')
